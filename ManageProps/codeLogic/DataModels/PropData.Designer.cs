009a363c376c418407e02f815db888d4475e41d3
Donald Skipper <delcarta@yahoo.com>
2021-01-27 02:21:25 -0500

    Changed target framework to ensure build success

--- ManageProps/codeLogic/DataModels/PropData.Designer.cs
+++ ManageProps/codeLogic/DataModels/PropData.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Delca\Documents\Visual Studio 2017\Projects\ManageProps\ManageProps\codeLogic\DataModels\PropData.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Delca\OneDrive\Documents\Repos\PropManage\ManageProps\codeLogic\DataModels\PropData.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
